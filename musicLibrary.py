41804ee88f5567b7bb9125fbeb7dbab385a07c8f
Nidhi Rajendra Sakpal <sakpalnidhi4@gmail.com>
2025-07-18 21:47:59 +0100

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,7 @@
+music = {
+    "stealth":"https://www.youtube.com/watch?v=U47Tr9BB_wE&list=RDU47Tr9BB_wE&start_radio=1&pp=ygUVc3RlYWx0aCB5b3V0dWJlIG11c2ljoAcB",
+    "wonders":"https://youtu.be/xqNGLIPY4Fs?list=PLFCHGavqRG-q_2ZhmgU2XB2--ZY6irT1c",
+    "happier":"https://youtu.be/kfCTO9PPtVo?list=PLqDyiQp0DOxCZ7GFMcKgTN5E_4NrLTCxi",
+    "staycation":"https://youtu.be/mJy2lq7fBzQ?list=PLFCHGavqRG-q_2ZhmgU2XB2--ZY6irT1c",
+    "skyfall" : "https://www.youtube.com/watch?v=DeumyOzKqgI&list=RDDeumyOzKqgI&start_radio=1&pp=ygUHc2t5ZmFsbKAHAQ%3D%3D"
+}
